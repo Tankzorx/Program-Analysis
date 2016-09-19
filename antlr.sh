5ea250e0801696cfca427e49f1a3a0eb26988271
Magnus Møller <magnus108@me.com>
2016-09-19 16:26:40 +0200

    antlr.sh quickfix

--- antlr.sh
+++ antlr.sh
@@ -1,4 +1,5 @@
 java -jar lib/antlr-4.5.3-complete.jar src/MicroC_language/parsing/MicroC.g4 
+mkdir bin
 javac -sourcepath src -cp lib/*.jar src/MicroC_language/MicroC.java src/MicroC_language/parsing/*.java -d bin
 java -cp ./lib/antlr-4.5.3-complete.jar:./bin MicroC ./tests/test1.microC
 
